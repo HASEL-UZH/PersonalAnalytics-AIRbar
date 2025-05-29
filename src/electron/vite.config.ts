4a3779a5d4573edc0ca4e51c38f5d67b827bf5dc
royru <roy.rutishauser@gmail.com>
2025-05-29 12:17:57 +0200

    reverse accidental changes

--- src/electron/vite.config.ts
+++ src/electron/vite.config.ts
@@ -1,27 +1,27 @@
-import fs from 'node:fs'
-import path from 'node:path' // ***AIRBAR 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import electron from 'vite-plugin-electron/simple'
-import alias from '@rollup/plugin-alias' // ***AIRBAR 
+import fs from 'node:fs';
+import path from 'node:path'; // ***AIRBAR 
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import electron from 'vite-plugin-electron/simple';
+import alias from '@rollup/plugin-alias'; // ***AIRBAR 
 import pkg from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
-  fs.rmSync('dist-electron', { recursive: true, force: true })
+  fs.rmSync('dist-electron', { recursive: true, force: true });
 
-  const selfReflectionDir = path.resolve(__dirname, 'PA.SelfReflection/src') // ***AIRBAR
+  const selfReflectionDir = path.resolve(__dirname, 'PA.SelfReflection/src'); // ***AIRBAR
 
-  const isServe = command === 'serve'
-  const isBuild = command === 'build'
-  const sourcemap = isServe || !!process.env.VSCODE_DEBUG
+  const isServe = command === 'serve';
+  const isBuild = command === 'build';
+  const sourcemap = isServe || !!process.env.VSCODE_DEBUG;  
 
   return {
     plugins: [
       // ***AIRBAR - START
       // this works for the renderer files, but does not work for the main files
       alias({
-        entries: [{ find: '@externalVue', replacement: selfReflectionDir }]
+        entries: [ { find: '@externalVue', replacement: selfReflectionDir } ]
       }),
       // ***AIRBAR - END
       vue(),
@@ -36,13 +36,13 @@ export default defineConfig(({ command }) => {
               outDir: 'dist-electron/main',
               rollupOptions: {
                 // ***AIRBAR - START
-                plugins:
+                plugins: 
                   // for the main files...
                   alias({
-                    entries: [{ find: '@external', replacement: selfReflectionDir }]
+                    entries: [ { find: '@external', replacement: selfReflectionDir } ]
                   }),
                 // ***AIRBAR - END
-
+                
                 // Some third-party Node.js libraries may not be built correctly by Vite, especially `C/C++` addons,
                 // we can use `external` to exclude them to ensure they work correctly.
                 // Others need to put them in `dependencies` to ensure they are collected into `app.asar` after the app is built.
@@ -70,5 +70,5 @@ export default defineConfig(({ command }) => {
       })
     ],
     clearScreen: false
-  }
-})
+  };
+});
